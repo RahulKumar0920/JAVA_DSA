5c70e857355d98bc67fd42f59c8f33ea5e2c87fd
Rahul Kumar <krrahul220919@gmail.com>
2025-01-19 23:30:27 +0530

    19_Jan_25

--- Arrays/src/UnionTwoArray.java
+++ Arrays/src/UnionTwoArray.java
@@ -1,14 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class UnionTwoArray {
     public static void main(String[] args){
 
-        int n = 5;
-        int m = 5;
-
-        int[] arr1 = {1,2,3,4,5};
-        int[] arr2 = {2,3,4,4,5};
+        int[] arr1 = {-7 ,8};
+        int[] arr2 = {-8 ,-3 ,8};
 
 
+        HashMap<Integer, Integer> frequency = new HashMap();
+        ArrayList<Integer> union = new ArrayList<>();
 
+        for(int i = 0; i < arr1.length; i++){
+            frequency.put(arr1[i],frequency.getOrDefault(arr1[i],0) + 1);
+        }
+
+
+        for(int j = 0; j < arr2.length; j++){
+            frequency.put(arr2[j], frequency.getOrDefault(arr2[j], 0) + 1);
+        }
+
+        for(int it : frequency.keySet()){
+            union.add(it);
+        }
+
+        union.sort(null);
+
+        for(int it : union){
+            System.out.print(it + " ");
+        }
 
     }
 }
